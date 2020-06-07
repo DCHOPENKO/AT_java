f8fb81efaeecf26f0458785e0d3bac52953d47da
Dmitriy Chopenko <dchopenko1986@gmail.com>
2020-06-07 21:15:36 +0300

    1

--- src/test/java/ru/geekbrains/main/site/at/SearchTest.java
+++ src/test/java/ru/geekbrains/main/site/at/SearchTest.java
@@ -1,74 +1,79 @@
 package ru.geekbrains.main.site.at;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-@Disabled
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class SearchTest extends BaseTest {
 
-
-    @Test
-    void postsProfessionsForSearchTest() {
+    @BeforeAll
+    public static void setUpVeforeAll() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='professions']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='professions']");
-        Assertions.assertTrue(value >= 2);
     }
 
-    @Test
-    void postsCoursesForSearchTest() {
-        searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='courses']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='courses']");
-        Assertions.assertTrue(value > 15);
+    @AfterAll
+    public static void tearDownAfterAll() {
+        driver.quit();
     }
 
-    @Test
-    void postsWebinarsForSearchTest() {
-        searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='webinars']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='webinars']");
-        Assertions.assertTrue(value > 180 && value < 300);
+    @Override
+    @BeforeEach
+    public void setUp() {
     }
 
-    @Test
-    void postsBlogsForSearchTest() {
-        searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='blogs']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='blogs']");
-        Assertions.assertTrue(value > 300);
+    @Override
+    @AfterEach
+    public void tearDown() {
     }
 
-    @Test
-    void postsForumsForSearchTest() {
-        searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='forums']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='forums']");
-        Assertions.assertTrue(value != 350);
+    @ParameterizedTest
+    @DisplayName("Проверка поискового запроса / проверка отображения инф. о найденных блоках (название и кол-во))")
+    @CsvFileSource(resources = "/SearchTest_source_data.csv", numLinesToSkip = 1)
+    void postsProfessionsForSearchTest(String name, String cssSelector) {
+        blockIsDisplayed(cssSelector);
+        checkValueCondition(cssSelector);
     }
 
-    @Test
-    void postsTestsForSearchTest() {
-        searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='tests']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='tests']");
-        Assertions.assertTrue(value != 0);
+    public void checkValueCondition(String title) {
+        int value = verifyQuantityPosts(title);
+        if (title.contains("professions")) {
+            assertThat(value, greaterThanOrEqualTo(2));
+        } else if (title.contains("courses")) {
+            assertThat(value, greaterThan(15));
+        } else if (title.contains("webinars")) {
+            assertThat(value, allOf(
+                    greaterThan(180),
+                    lessThan(300)
+            ));
+        } else if (title.contains("blogs")) {
+            assertThat(value, greaterThan(300));
+        } else if (title.contains("forums")) {
+            assertThat(value, not(equalTo(350)));
+        } else if (title.contains("tests")) {
+            assertThat(value, not(equalTo(0)));
+        } else if (title.contains("companies")) {
+            assertThat(value, not(equalTo(0)));
+        }
     }
 
-    @Test
-    void postsCompaniesForSearchTest() {
-        searchRequest("java", 15);
-        blockIsDisplayed("ul.search-page-tabs > li > a[data-tab='companies']");
-        int value = verifyQuantityPosts("ul.search-page-tabs > li > a[data-tab='companies']");
-        Assertions.assertTrue(value != 0);
-    }
-
-    void searchRequest (String searchRequest, int waitingTimeInSec) {
+    public static void searchRequest(String searchRequest, int waitingTimeInSec) {
         driver.get(BASE_URL + "/courses");
         driver.findElement(By.xpath("//div/div/button[*]")).click();
         driver.findElement(By.cssSelector("ul > li > .show-search-form")).click();
@@ -88,5 +93,4 @@ public class SearchTest extends BaseTest {
         String[] array = text.split("・");
         return Integer.parseInt(array[array.length - 1]);
     }
-
 }
